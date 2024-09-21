6e1f96274f7b0ba2e307a7534fbcc5c6a347ecb0
Büşra Göktaş <busra1gkts@gmail.com>
2024-09-21 11:23:23 +0300

    Create DbCoontext

---
+++ ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repositories.EFCore;
+
+namespace WebApı.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            // ConfigurationBuilder
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            // DbContextOptionsBuilder
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+                prj => prj.MigrationsAssembly("WebApı"));
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
